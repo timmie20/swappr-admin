933ec8953f602664440510e512b660e4e09efc49
timmie20 <olutimilehinolayinka1234@gmail.com>
2026-01-01 12:22:32 +0100

    chore: stage pending changes before sync

--- src/features/auth/components/sign-in-view.tsx
+++ src/features/auth/components/sign-in-view.tsx
@@ -1,6 +1,6 @@
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { SignIn as ClerkSignInForm, SignUp } from '@clerk/nextjs';
+import { SignIn as ClerkSignInForm } from '@clerk/nextjs';
 
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { IconStar } from '@tabler/icons-react';
